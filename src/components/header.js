f262e0197d50ec92c93429cb0480f3853ad8df9e
Deng Changdong <dengchangdong@gmail.com>
2023-11-04 10:52:47 +0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -19,8 +19,8 @@ function Header() {
       </div>
       <div className='status-top'>
         <div className='status-tip' id='status-tip'></div>
-        <p className='status-text' id='status-text'>站点状态加载中</p>
-        <p className='status-text' id='status-down'>部分站点无法运行</p>
+        <p className='status-text' id='status-text'>服务状态加载中</p>
+        <p className='status-text' id='status-down'>部分服务无法运行</p>
         <p className='status-time' id='status-time-up'>上次更新于&nbsp;<span id='status-last-time'>00&nbsp;:&nbsp;00</span>&emsp;|&emsp;检测频率&nbsp;5&nbsp;分钟</p>
       </div>
     </div>
